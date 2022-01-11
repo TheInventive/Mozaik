36ddd8d0135b77393cb6bd7d2f4fff63ec5615c1
hosszuerik <hosszu.erik@std.t-systems.hu>
2022-01-11 11:13:18 +0100

    commit 4

--- Html/index.php
+++ Html/index.php
@@ -7,47 +7,51 @@
     <title>Title</title>
 </head>
 <body>
-<form id="form" action="">
-    <div class="dropdown">
+<form action="">
+    <div class="flex-container">
         <h1 class="h">Megye:</h1>
-        <button class="dropbtn" id="choice">Válasszon!</button>
-        <div class="dropdown-content">
+        <div class="dropdown">
+            <button class="dropbtn" id="choice" disabled>Válasszon!</button>
+            <div class="dropdown-content">
 
-            <?php
-            include "../Php/connection.php";
-            $sql = "select * from mozaik.megyek;";
-
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-
-                $i = 0;
-                while($row = $result->fetch_assoc()) {
-                    $i++;
-                    echo '<form method="post"><button id="'.$row["megyeid"].'">'. $row["megyenev"].'</input></form><br>';
-                }
-                global $i;
-
-            }else {
-                echo "Hiba!";
-            }
-            $conn->close();
-
-            ?>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-            <script src="../Js/2.js"></script>
-            <script src="../Js/GetName.js"></script>
-            <script>
                 <?php
+                include "../Php/connection.php";
+                $sql = "select * from mozaik.megyek;";
+
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+
+                    $i = 0;
+                    while($row = $result->fetch_assoc()) {
+                        $i++;
+                        echo '<form method="post"><button id="'.$row["megyeid"].'">'. $row["megyenev"].'</input></form><br>';
+                    }
+                    global $i;
+
+                }else {
+                    echo "Hiba!";
+                }
+                $conn->close();
+
+                ?>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+                <script src="../Js/2.js"></script>
+                <script src="../Js/GetName.js"></script>
+                <script>
+                    <?php
                     for($j = 1; $j < $i; $j++){
                         echo 'document.getElementById(\''.($j).'\').addEventListener(\'click\', getName);';
                     }?>
-            </script>
+                </script>
+            </div>
+        </div>
+        <h1 class="h">Megye:</h1>
+        <div>
+            <h1 class="h" id="megye"></h1>
+            <div id="result"></div>
         </div>
     </div>
-    <h1 class="h">Megye:</h1>
-    <h1 class="h" id="megye"></h1>
-    <div id="result"></div>
 </form>
 </body>
 </html>
